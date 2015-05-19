7a51afa4b44d0a2e4ff1472312b472f87acc3597
Adriano Tadao <drianotadao@gmail.com>
2015-05-18 21:47:41 -0300

    I wrote some tests

--- test/fragment_cache_test.rb
+++ test/fragment_cache_test.rb
@@ -1,19 +1,27 @@
 require File.expand_path '../test_helper.rb', __FILE__
 
 describe Sinatra::FragmentCache do
-  let(:cache_path){ "#{ FileUtils.pwd }/test/dummy/tmp/cache" }
-
-  after do
-    FileUtils.rm_r cache_path, force: true
+  before do
+    app.set :fragment_cache_output_dir, "#{ FileUtils.pwd }/test/dummy/tmp/cache"
   end
 
-  it "When the file_name in nil, the cache's name should be index.cache" do
+  after do
+    FileUtils.rm_r app.fragment_cache_output_dir, force: true
+  end
+
+  it "When the file_name is nil, the cache's name should be index.cache" do
     get '/', { file_name: nil }
-    File.file?("#{ cache_path }/cache_key/index.cache").must_equal true
+    File.file?("#{ app.fragment_cache_output_dir }/cache_key/index.cache").must_equal true
   end
 
   it "When the file_name isn't nil, the cache's name shouldn't be index.cache" do
     get '/', { file_name: 'index.html' }
-    File.file?("#{ cache_path }/cache_key/index.html").must_equal true
+    File.file?("#{ app.fragment_cache_output_dir }/cache_key/index.html").must_equal true
+  end
+
+  it "When the fragment cache_path is changed, the path should be created on the same" do
+    app.set :fragment_cache_output_dir, "#{ FileUtils.pwd }/public/cache"
+    get '/', { file_name: 'index.html' }
+    File.file?("#{ app.fragment_cache_output_dir }/cache_key/index.html").must_equal true
   end
 end
